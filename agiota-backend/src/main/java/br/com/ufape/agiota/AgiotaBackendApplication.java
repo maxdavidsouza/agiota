dfeaa34bc16c5495b2176e9f0aa6276fc5295403
david <david@DESKTOP-AFK4BSC>
2024-07-06 13:17:05 -0300

    Inicialização do Projeto Spring Boot

---
+++ agiota-backend/src/main/java/br/com/ufape/agiota/AgiotaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufape.agiota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgiotaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgiotaBackendApplication.class, args);
+	}
+
+}
