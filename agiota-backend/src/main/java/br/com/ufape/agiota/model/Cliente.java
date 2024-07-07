cd62d703ef793918ea6c4c245d87a8902ab9169c
Luiz Felipe <luizfelipe.ssu@gmail.com>
2024-07-07 17:08:49 -0300

    Adicionando Classes Básicas de Negócio (parte 2)

---
+++ agiota-backend/src/main/java/br/com/ufape/agiota/model/Cliente.java
@@ -0,0 +1,20 @@
+package br.com.ufape.agiota.model;
+
+public class Cliente {
+
+	public void avaliarAgiota() {
+		
+	}
+	
+	public void aceitarEmprestimo() {
+		
+	}
+	
+	public void pagarEmprestimo() {
+		
+	}
+	
+	public void gerarLembrete() {
+		
+	}
+}
