413275446a075febf7678dbafd50cef01b532393
Max David <maxdavidsouza@gmail.com>
2024-08-07 00:58:59 -0300

    Adicionando primeiras páginas de teste

---
+++ agiota-next-frontend/app/agiotas/page.js
@@ -0,0 +1,9 @@
+export default function Agiotas(){
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                Página dos Agiotas
+            </div>
+        </main>
+    )
+}
