27773932842247973207c4cce26b848fa38bfcb5
mvinicius175 <mauro.vinicius175@gmail.com>
2024-09-26 09:47:28 -0300

    Estilização da página de dados da parcela de cliente

--- agiota-next-frontend/app/clientes/[id]/emprestimos/[emprestimo_id]/parcelas/[parcela_id]/page.js
+++ agiota-next-frontend/app/clientes/[id]/emprestimos/[emprestimo_id]/parcelas/[parcela_id]/page.js
@@ -14,22 +14,30 @@ export default async function CarregarParcela({params}) {
   }
     if(parcela != null) {
       return (
-        <main className="flex flex-col items-center">
-            <h1>Dados da Parcela {parcela.id}</h1>
-            <p>Valor Emprestado: {parcela.valorASerPago}</p>
-            <p>Data de Vencimento: {parcela.dataDeVencimento}</p>
-            <p>Estado: {parcela.estado}</p>
-            {parcela.valorPago != 0 && (
-            <p>Valor Devolvido: {parcela.valorPago}</p>
-            )}
-            {parcela.dataDePagamento != null && (
-            <p>Data de Pagamento: {parcela.dataDePagamento}</p>
-            )}
-            {parcela.multa != null && (
-            <p>Valor da multa: {parcela.multa.valor}</p>
-            )}
-            <Link href={`/clientes/${params.id}/emprestimos/${params.emprestimo_id}/parcelas/${params.parcela_id}/pagamento`}>Pagar Parcela</Link>
-            <Link href={`/clientes/${params.id}/emprestimos/${params.emprestimo_id}/parcelas/${params.parcela_id}/lembrete`}>Gerar Lembrete</Link>
+        <main className="flex flex-col items-center p-4 bg-gray-100 min-h-screen">
+        <h1 className="text-2xl font-bold mb-4">Dados da Parcela {parcela.id}</h1>
+        <div className="bg-[#007ea7] text-white shadow-md rounded-lg p-6 w-full max-w-md">
+        <p className="mb-2"><span className="font-semibold">Valor Emprestado:</span> {parcela.valorASerPago}</p>
+        <p className="mb-2"><span className="font-semibold">Data de Vencimento:</span> {parcela.dataDeVencimento}</p>
+        <p className="mb-2"><span className="font-semibold">Estado:</span> {parcela.estado}</p>
+        {parcela.valorPago != 0 && (
+        <p className="mb-2"><span className="font-semibold">Valor Devolvido:</span> {parcela.valorPago}</p>
+        )}
+        {parcela.dataDePagamento != null && (
+        <p className="mb-2"><span className="font-semibold">Data de Pagamento:</span> {parcela.dataDePagamento}</p>
+        )}
+        {parcela.multa != null && (
+        <p className="mb-2"><span className="font-semibold">Valor da multa:</span> {parcela.multa.valor}</p>
+        )}
+        <div className="flex justify-between mt-4">
+        <Link href={`/clientes/${params.id}/emprestimos/${params.emprestimo_id}/parcelas/${params.parcela_id}/pagamento`} className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 no-underline">
+          Pagar Parcela
+        </Link>
+        <Link href={`/clientes/${params.id}/emprestimos/${params.emprestimo_id}/parcelas/${params.parcela_id}/lembrete`} className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 no-underline">
+          Gerar Lembrete
+        </Link>
+        </div>
+        </div>
         </main>
       );
     } else {
